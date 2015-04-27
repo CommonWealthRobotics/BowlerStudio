18e860870d12ce21c671c9a90b65f0abcf88a9db
madhephaestus <mad.hephaestus@gmail.com>
2015-04-27 13:18:20 -0700

    adding windows build information

--- src/main/java/com/neuronrobotics/jniloader/OpenCVJNILoader.java
+++ src/main/java/com/neuronrobotics/jniloader/OpenCVJNILoader.java
@@ -42,11 +42,10 @@ public class OpenCVJNILoader {
 		if(NativeResource.isOSX())
 			resource.load("lib"+Core.NATIVE_LIBRARY_NAME);
 		else if(NativeResource.isWindows()){
-			String dir = "OpenCV-"+Core.VERSION.split(".0")[0];
 			if(NativeResource.is64Bit()){
-				System.load("C:\\"+dir+"\\build\\java\\x64\\"+Core.NATIVE_LIBRARY_NAME+".dll");
+				System.load("C:\\OpenCV\\build\\java\\x64\\"+Core.NATIVE_LIBRARY_NAME+".dll");
 			}else{
-				System.load("C:\\"+dir+"\\build\\java\\x86\\"+Core.NATIVE_LIBRARY_NAME+".dll");
+				System.load("C:\\OpenCV\\build\\java\\x86\\"+Core.NATIVE_LIBRARY_NAME+".dll");
 			}
 		}
 		
