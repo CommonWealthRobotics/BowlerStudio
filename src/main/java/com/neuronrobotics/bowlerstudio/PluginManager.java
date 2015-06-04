3c0aba08c2fd952a558ab4ff103e11d6397fa7d2
hephaestus <mad.hephaestus@gmail.com>
2015-06-03 20:25:06 -0400

    adding the sailance detector back intot he plugins

--- src/main/java/com/neuronrobotics/bowlerstudio/PluginManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/PluginManager.java
@@ -21,6 +21,7 @@ import javafx.stage.StageStyle;
 import com.neuronrobotics.bowlerstudio.tabs.AbstractBowlerStudioTab;
 import com.neuronrobotics.bowlerstudio.tabs.CameraTab;
 import com.neuronrobotics.jniloader.AbstractImageProvider;
+import com.neuronrobotics.jniloader.SalientDetector;
 import com.neuronrobotics.nrconsole.plugin.BowlerCam.BowlerCamController;
 import com.neuronrobotics.nrconsole.plugin.DeviceConfig.PrinterConiguration;
 import com.neuronrobotics.nrconsole.plugin.DyIO.DyIOConsole;
@@ -75,6 +76,7 @@ public class PluginManager {
 		
 		if(AbstractImageProvider.class.isInstance(dev)){
 			deviceSupport.add(CameraTab.class);
+			deviceSupport.add(SalientDetector.class);
 		}
 		
 		if(NRBootLoader.class.isInstance(dev)){
