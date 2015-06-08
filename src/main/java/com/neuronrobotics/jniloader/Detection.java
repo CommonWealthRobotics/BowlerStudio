b6e7b3f14228726398872dca14641255b7bc353a
hephaestus <mad.hephaestus@gmail.com>
2015-06-08 15:49:05 -0400

    adding the confidence parameters

--- src/main/java/com/neuronrobotics/jniloader/Detection.java
+++ src/main/java/com/neuronrobotics/jniloader/Detection.java
@@ -5,11 +5,26 @@ public class Detection {
 	private final double X;
 	private final double Y;
 	private final double Size;
+	private double confidence;
 	
 	public Detection(double x, double y, double size){
 		X=x;
 		Y=y;
 		Size=size;
+		this.setConfidence(1.0);
+	}
+	/**
+	 * 
+	 * @param x detection location in image
+	 * @param y detection location in image
+	 * @param size detection location in image
+	 * @param confidence confidence value from 0-1 of confidence in detection
+	 */
+	public Detection(double x, double y, double size, double confidence){
+		X=x;
+		Y=y;
+		Size=size;
+		this.setConfidence(confidence);
 	}
 
 	public double getSize() {
@@ -25,5 +40,13 @@ public class Detection {
 	}
 	public String toString(){
 		return "X pos ="+X+" Y pos="+Y+" size="+Size; 
+	}
+
+	public double getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(double confidence) {
+		this.confidence = confidence;
 	}
 }
