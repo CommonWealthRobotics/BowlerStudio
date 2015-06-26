2779c78253d52d40e7dad84cc195cb5e9f286092
hephaestus <mad.hephaestus@gmail.com>
2015-06-26 16:32:26 -0400

    changable device names from UI

--- src/main/java/com/neuronrobotics/bowlerstudio/PluginManagerWidget.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/PluginManagerWidget.java
@@ -2,10 +2,12 @@ package com.neuronrobotics.bowlerstudio;
 
 import com.neuronrobotics.sdk.common.DeviceManager;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.CheckBoxTreeCell;
@@ -14,24 +16,31 @@ import javafx.scene.layout.HBox;
 public class PluginManagerWidget extends HBox {
 	private PluginManager manager;
 	private TreeItem<String> rootItem;
+	private TextField deviceName = new TextField();
+	private Button disconnectAll;
 
 	public PluginManagerWidget(PluginManager manager, Node graphic){
 		super(20);
 		this.setManager(manager);
-		rootItem = new TreeItem<String>(manager.getName(), graphic);
+		rootItem = new TreeItem<String>(manager.getDevice().getClass().getSimpleName(), graphic);
 		TreeView<String> tree = new TreeView<String>(rootItem);
 		manager.setTree(rootItem);
 		tree.setCellFactory(CheckBoxTreeCell.forTreeView());
 		rootItem.setExpanded(false);
-		Button disconnectAll = new Button("Disconnect "+manager.getName());
+		disconnectAll = new Button("Disconnect "+manager.getName());
 		disconnectAll.setOnAction(new EventHandler<ActionEvent>() {
 		    @Override public void handle(ActionEvent e) {
 		    	getManager().getDevice().disconnect();
 		    	DeviceManager.remove(getManager().getDevice());
 		    }
 		});
+		deviceName.setOnAction(event -> {
+			getManager().setName(deviceName.getText());
+			disconnectAll.setText("Disconnect "+manager.getName());
+		});
+		Platform.runLater(()->deviceName.setText(manager.getName()));
 		
-		getChildren().addAll(disconnectAll,tree);
+		getChildren().addAll(disconnectAll,deviceName,tree);
 		
 	}
 
