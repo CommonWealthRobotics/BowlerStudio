035f4bf2e23b7d446f9e3acae3b86535da541ce8
hephaestus <mad.hephaestus@gmail.com>
2015-07-01 19:13:30 -0400

    adding the link editor widgets

--- src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHKinematicsLab.java
+++ src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHKinematicsLab.java
@@ -1,10 +1,16 @@
 package com.neuronrobotics.nrconsole.plugin.cartesian;
 
+import javafx.scene.layout.VBox;
+
 import com.neuronrobotics.bowlerstudio.tabs.AbstractBowlerStudioTab;
+import com.neuronrobotics.sdk.addons.kinematics.DHLink;
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
 import com.neuronrobotics.sdk.common.BowlerAbstractDevice;
 
 public class DHKinematicsLab extends AbstractBowlerStudioTab {
-
+	DHParameterKinematics device;
+	
+	
 	@Override
 	public void onTabClosing() {
 		// TODO Auto-generated method stub
@@ -20,7 +26,12 @@ public class DHKinematicsLab extends AbstractBowlerStudioTab {
 	@Override
 	public void initializeUI(BowlerAbstractDevice pm) {
 		// TODO Auto-generated method stub
-
+		device=(DHParameterKinematics)pm;
+		VBox links = new VBox();
+		for(DHLink l:device.getChain().getLinks()){
+			links.getChildren().add(new DHLinkWidget(l));
+		}
+		setContent(links);
 	}
 
 	@Override
---
+++ src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHLinkWidget.java
@@ -0,0 +1,12 @@
+package com.neuronrobotics.nrconsole.plugin.cartesian;
+
+import com.neuronrobotics.sdk.addons.kinematics.DHLink;
+
+import javafx.scene.Group;
+
+public class DHLinkWidget extends Group{
+
+	public DHLinkWidget(DHLink l) {
+		// TODO Auto-generated constructor stub
+	}
+}
