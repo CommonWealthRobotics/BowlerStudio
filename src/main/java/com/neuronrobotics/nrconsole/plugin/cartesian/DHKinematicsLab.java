d79f9f7335f0f5b5d7c1ae9ceecf43f66624284e
hephaestus <mad.hephaestus@gmail.com>
2015-07-01 21:17:42 -0400

    basic dh- lab is working

--- src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHKinematicsLab.java
+++ src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHKinematicsLab.java
@@ -1,15 +1,27 @@
 package com.neuronrobotics.nrconsole.plugin.cartesian;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import sun.security.action.GetLongAction;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
 import com.neuronrobotics.bowlerstudio.tabs.AbstractBowlerStudioTab;
+import com.neuronrobotics.nrconsole.util.FileSelectionFactory;
+import com.neuronrobotics.nrconsole.util.GroovyFilter;
 import com.neuronrobotics.sdk.addons.kinematics.DHLink;
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
 import com.neuronrobotics.sdk.common.BowlerAbstractDevice;
+import com.neuronrobotics.sdk.common.Log;
 
 public class DHKinematicsLab extends AbstractBowlerStudioTab {
 	DHParameterKinematics device;
-	
+	private File currentFile=null;
 	
 	@Override
 	public void onTabClosing() {
@@ -20,16 +32,45 @@ public class DHKinematicsLab extends AbstractBowlerStudioTab {
 	@Override
 	public String[] getMyNameSpaces() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String[0];
 	}
 
 	@Override
 	public void initializeUI(BowlerAbstractDevice pm) {
 		// TODO Auto-generated method stub
+		setText("DH Lab");
 		device=(DHParameterKinematics)pm;
-		VBox links = new VBox();
-		for(DHLink l:device.getChain().getLinks()){
-			links.getChildren().add(new DHLinkWidget(l));
+		Log.debug("Loading xml: "+device.getXml());
+		VBox links = new VBox(20);
+		Button save = new Button("Save Configuration");
+		save.setOnAction(event -> {
+			new Thread(){
+				public void run(){
+					File last = FileSelectionFactory.GetFile(currentFile==null?
+										ScriptingEngine.getWorkspace():
+										new File(ScriptingEngine.getWorkspace().getAbsolutePath()+"/"+currentFile.getName()),
+							new GroovyFilter());
+					if (last != null) {
+						try {
+							Files.write(Paths.get(last.getAbsolutePath()),device.getXml().getBytes() );
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}.start();
+		});
+		ArrayList<DHLink> dhLinks = device.getChain().getLinks();
+		links.getChildren().add(save);
+		for(int i=0;i<dhLinks.size();i++){
+			
+			links.getChildren().add(
+									new DHLinkWidget(i,
+													dhLinks.get(i),
+													device
+													)
+									);
 		}
 		setContent(links);
 	}
--- src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHLinkWidget.java
+++ src/main/java/com/neuronrobotics/nrconsole/plugin/cartesian/DHLinkWidget.java
@@ -1,12 +1,133 @@
 package com.neuronrobotics.nrconsole.plugin.cartesian;
 
+import com.neuronrobotics.sdk.addons.kinematics.AbstractLink;
 import com.neuronrobotics.sdk.addons.kinematics.DHLink;
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
+import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration;
+import com.neuronrobotics.sdk.common.Log;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
 public class DHLinkWidget extends Group{
+	public DHLinkWidget(int linkIndex, DHLink dhLink, DHParameterKinematics device ) {
 
-	public DHLinkWidget(DHLink l) {
-		// TODO Auto-generated constructor stub
+		HBox panel = new HBox(10);
+		AbstractLink abstractLink  = device.getAbstractLink(linkIndex);
+		
+		
+		TextField delta = new TextField(""+dhLink.getDelta());
+		delta.setOnAction(event -> {
+			dhLink.setDelta(Double.parseDouble(delta.getText()));
+			device.getCurrentTaskSpaceTransform();
+		});
+		
+		Slider theta = new Slider();
+		theta.setMin(-180);
+		theta.setMax(180);
+		theta.setValue(Math.toDegrees(dhLink.getTheta()));
+		theta.setShowTickLabels(true);
+		theta.setShowTickMarks(true);
+		theta.setMajorTickUnit(50);
+		theta.setMinorTickCount(5);
+		theta.setBlockIncrement(10);
+		theta.setSnapToTicks(true);
+		final Label thetaValue = new Label(
+		        Double.toString(theta.getValue()));
+		theta.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                    Number old_val, Number new_val) {
+            		dhLink.setTheta(Math.toRadians(new_val.doubleValue()));
+            		device.getCurrentTaskSpaceTransform();
+
+            		thetaValue.setText(Double.toString(theta.getValue()));
+                }
+        });
+		
+		TextField radius = new TextField(""+dhLink.getRadius());
+		radius.setOnAction(event -> {
+			dhLink.setRadius(Double.parseDouble(radius.getText()));
+			device.getCurrentTaskSpaceTransform();
+		});
+		
+		Slider Alpha = new Slider();
+		Alpha.setMin(-180);
+		Alpha.setMax(180);
+		Alpha.setValue(Math.toDegrees(dhLink.getAlpha()));
+		Alpha.setShowTickLabels(true);
+		Alpha.setShowTickMarks(true);
+		Alpha.setMajorTickUnit(50);
+		Alpha.setMinorTickCount(5);
+		Alpha.setBlockIncrement(10);
+		Alpha.setSnapToTicks(true);
+		final Label AlphaValue = new Label(
+		        Double.toString(Alpha.getValue()));
+		Alpha.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                    Number old_val, Number new_val) {
+            		dhLink.setAlpha(Math.toRadians(new_val.doubleValue()));
+            		device.getCurrentTaskSpaceTransform();
+            		AlphaValue.setText(Double.toString(Alpha.getValue()));
+                }
+        });
+		
+		TextField name = new TextField(abstractLink.getLinkConfiguration().getName());
+		name.setOnAction(event -> {
+			abstractLink.getLinkConfiguration().setName(name.getText());
+		});
+		
+		Slider setpoint = new Slider();
+		setpoint.setMin(abstractLink.getMinEngineeringUnits());
+		setpoint.setMax(abstractLink.getMaxEngineeringUnits());
+		setpoint.setValue(0);
+		setpoint.setShowTickLabels(true);
+		setpoint.setShowTickMarks(true);
+		setpoint.setSnapToTicks(true);
+		setpoint.setMajorTickUnit(50);
+		setpoint.setMinorTickCount(5);
+		setpoint.setBlockIncrement(10);
+		final Label setpointValue = new Label(
+		        Double.toString(setpoint.getValue()));
+		setpoint.valueProperty().addListener(new ChangeListener<Number>() {
+
+            public void changed(ObservableValue<? extends Number> ov,
+                    Number old_val, Number new_val) {
+            		try {
+            			
+        				device.setDesiredJointAxisValue(linkIndex, new_val.doubleValue(), 0);
+        				Log.debug("Setting the setpoint on #"+linkIndex+" to "+new_val);
+        				setpointValue.setText(Double.toString(setpoint.getValue()));
+        			} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					};
+                }
+        });
+		
+		
+		
+		
+		
+		
+		panel.getChildren().addAll(	new Text("#"+linkIndex),
+									name,
+									setpoint,
+									setpointValue,
+									new Text("Delta"),
+									delta,
+									new Text("Theta"),
+									theta,thetaValue,
+									new Text("Radius"),
+									radius,
+									new Text("Alpha"),
+									Alpha,AlphaValue
+									);
+		getChildren().add(panel);
 	}
 }
