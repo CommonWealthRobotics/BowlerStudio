5a8be6ef282bb42ed1b178af2fac3a3222e88977
hephaestus <mad.hephaestus@gmail.com>
2015-08-23 11:48:46 -0400

    changing default jog to a more reasonable 30ms

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/JogWidget.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/JogWidget.java
@@ -40,8 +40,8 @@ public class JogWidget extends GridPane implements ITaskSpaceUpdateListenerNR, I
 	Button nz = new Button("-Z");
 	Button home = new Button("home");
 	Button game = new Button("Add Game Controller");
-	TextField increment=new TextField("10");
-	TextField sec=new TextField(".01");
+	TextField increment=new TextField("5");
+	TextField sec=new TextField(".03");
 	private TransformWidget transform;
 	BowlerJInputDevice gameController=null;
 	double x,y,rz,slider=0;
