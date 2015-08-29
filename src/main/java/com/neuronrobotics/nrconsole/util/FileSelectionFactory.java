69a7a746dee9e0af3446c49f7f939885412108bc
hephaestus <mad.hephaestus@gmail.com>
2015-08-29 00:40:19 -0400

    all file windows now open the file chooser for the OS

--- src/main/java/com/neuronrobotics/nrconsole/util/FileSelectionFactory.java
+++ src/main/java/com/neuronrobotics/nrconsole/util/FileSelectionFactory.java
@@ -2,6 +2,7 @@ package com.neuronrobotics.nrconsole.util;
 
 import java.io.File;
 
+import javafx.application.Platform;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
@@ -10,14 +11,42 @@ import javax.swing.filechooser.FileFilter;
 
 import com.neuronrobotics.bowlerstudio.BowlerStudio;
 import com.neuronrobotics.imageprovider.StaticFileProvider;
+import com.neuronrobotics.sdk.util.ThreadUtil;
 
 
 public class FileSelectionFactory {
+
 	public static File GetFile(File start, ExtensionFilter... filter) {
-		FileChooser fileChooser = new FileChooser();
-		fileChooser.getExtensionFilters().addAll(filter);
-		fileChooser.setTitle("Bowler File Chooser");
-		return fileChooser.showOpenDialog(BowlerStudio.getPrimaryStage());
+		class fileHolder{
+			private boolean done=false;
+			private File file=null;
+			public boolean isDone() {
+				return done;
+			}
+			public void setDone(boolean done) {
+				this.done = done;
+			}
+			public File getFile() {
+				return file;
+			}
+			public void setFile(File file) {
+				this.file = file;
+			}
+		}
+		final fileHolder file=new fileHolder();
+		Platform.runLater(() -> {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.getExtensionFilters().addAll(filter);
+			fileChooser.setTitle("Bowler File Chooser");
+			file.setFile(fileChooser.showOpenDialog(BowlerStudio.getPrimaryStage()));
+			file.setDone(true);
+			
+		});
+		while(!file.isDone()){
+			ThreadUtil.wait(16);
+		}
+			
+		return file.getFile();
 	}
 
 }
