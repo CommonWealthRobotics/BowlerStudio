17be1f078e8153713b58aa721c2738df91cb5117
hephaestus <mad.hephaestus@gmail.com>
2015-09-05 18:05:30 -0400

    new DyIO panel

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/tabs/DyIOControl.java
@@ -0,0 +1,57 @@
+package com.neuronrobotics.bowlerstudio.tabs;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import com.neuronrobotics.bowlerstudio.BowlerStudio;
+import com.neuronrobotics.sdk.common.BowlerAbstractDevice;
+import com.neuronrobotics.sdk.dyio.DyIO;
+
+public class DyIOControl extends AbstractBowlerStudioTab {
+
+	private DyIO dyio;
+	DyIOPanel controller ;
+	@Override
+	public void onTabClosing() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String[] getMyNameSpaces() {
+		// TODO Auto-generated method stub
+		return new String[]{"neuronrobotics.dyio.*"};
+	}
+
+	@Override
+	public void initializeUI(BowlerAbstractDevice pm) {
+		this.dyio = (DyIO)pm;
+		setText(dyio.getScriptingName());
+		FXMLLoader fxmlLoader = new FXMLLoader(
+                BowlerStudio.class.getResource("DyIOPanel.fxml"));
+        try {
+            fxmlLoader.load();
+        } catch (IOException ex) {
+            Logger.getLogger(BowlerStudio.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        Parent root = fxmlLoader.getRoot();
+        controller = fxmlLoader.getController();
+        controller.setDyIO(dyio,root);
+        setContent(root);
+	}
+	
+
+	@Override
+	public void onTabReOpening() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
