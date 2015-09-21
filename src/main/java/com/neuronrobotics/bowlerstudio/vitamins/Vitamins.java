2b683bf37ae906ff1792b7b868647d0bbb1e395f
kevin harrington <mad.hephaestus@gmail.com>
2015-09-21 11:21:21 -0400

    stroing vitamin files into a hashmap for quick retrieval.

--- src/main/java/com/neuronrobotics/bowlerstudio/vitamins/Vitamins.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/vitamins/Vitamins.java
@@ -2,6 +2,8 @@ package com.neuronrobotics.bowlerstudio.vitamins;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.neuronrobotics.imageprovider.NativeResource;
 
@@ -9,22 +11,32 @@ import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.STL;
 
 public class Vitamins {
+	
+	private static final Map<String,CSG> fileLastLoaded = new HashMap<String,CSG>();
+
 	public static CSG get(String resource ){
+		if(fileLastLoaded.get(resource) ==null ){
+			// forces the first time the files is accessed by the application tou pull an update
 			try {
-				return  STL.file(NativeResource.inJarLoad(IVitamin.class,resource).toPath());
+				fileLastLoaded.put(resource,STL.file(NativeResource.inJarLoad(IVitamin.class,resource).toPath()) );
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		return null;
+		}
+		return fileLastLoaded.get(resource).clone() ;
 	}
 	public static CSG get(File resource ){
-		try {
-			return  STL.file(resource.toPath());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		
+		if(fileLastLoaded.get(resource.getAbsolutePath()) ==null ){
+			// forces the first time the files is accessed by the application tou pull an update
+			try {
+				fileLastLoaded.put(resource.getAbsolutePath(), STL.file(resource.toPath()) );
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	return null;
+		return fileLastLoaded.get(resource).clone() ;
 	}
 }
