4aaed35bada3e7e93d42f10ff771fc219396370d
madhephaestus <mad.hephaestus@gmail.com>
2016-01-27 23:15:57 -0500

    adding a simple find feature for #8
    A sample from
    http://stackoverflow.com/questions/13437865/java-scroll-to-specific-text-inside-jtextarea/13438455#13438455
    was used to make a simple find feature.

--- src/main/java/com/neuronrobotics/bowlerstudio/tabs/LocalFileScriptTab.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/tabs/LocalFileScriptTab.java
@@ -1,8 +1,11 @@
 package com.neuronrobotics.bowlerstudio.tabs;
 
 import java.awt.Color;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
@@ -31,6 +34,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Document;
 import javax.swing.text.Highlighter;
 import javax.swing.text.Highlighter.HighlightPainter;
 
@@ -74,7 +78,7 @@ public class LocalFileScriptTab extends VBox implements IScriptEventListener, Ev
 	private Highlighter highlighter;
 
 	private HighlightPainter painter;
-
+	private int pos = 0;
 
     
 	public LocalFileScriptTab( File file) throws IOException {
@@ -126,6 +130,18 @@ public class LocalFileScriptTab extends VBox implements IScriptEventListener, Ev
             	getScripting().addIScriptEventListener(l);
 	        }
 	    });
+		
+		textArea.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					highlighter.removeAllHighlights();
+				}
+
+//				System.out.println("Number of click: " + e.getClickCount());
+//				System.out.println("Click position (X, Y):  " + e.getX() + ", " + e.getY());
+			}
+		});
+
 		sp = new RTextScrollPane(textArea);
 		
 		sn = new SwingNode();
@@ -150,7 +166,52 @@ public class LocalFileScriptTab extends VBox implements IScriptEventListener, Ev
 					textArea.getInputMap().put(keystroke, "f");
 					textArea.getActionMap().put("f", new AbstractAction() {
 						public void actionPerformed(ActionEvent e) {
-							System.out.println("Got ctrl f");
+							//System.out.println("Got ctrl f "+ textArea.getSelectedText());
+						      // Get the text to find...convert it to lower case for eaiser comparision
+		                    String find = textArea.getSelectedText().toLowerCase();
+		                    // Focus the text area, otherwise the highlighting won't show up
+		                    textArea.requestFocusInWindow();
+		                    // Make sure we have a valid search term
+		                    if (find != null && find.length() > 0) {
+		                        Document document = textArea.getDocument();
+		                        int findLength = find.length();
+		                        try {
+		                            boolean found = false;
+		                            // Rest the search position if we're at the end of the document
+		                            if (pos + findLength > document.getLength()) {
+		                                pos = 0;
+		                            }
+		                            // While we haven't reached the end...
+		                            // "<=" Correction
+		                            while (pos + findLength <= document.getLength()) {
+		                                // Extract the text from teh docuemnt
+		                                String match = document.getText(pos, findLength).toLowerCase();
+		                                // Check to see if it matches or request
+		                                if (match.equals(find)) {
+		                                    found = true;
+		                                    break;
+		                                }
+		                                pos++;
+		                            }
+
+		                            // Did we find something...
+		                            if (found) {
+		                                // Get the rectangle of the where the text would be visible...
+		                                Rectangle viewRect = textArea.modelToView(pos);
+		                                // Scroll to make the rectangle visible
+		                                textArea.scrollRectToVisible(viewRect);
+		                                // Highlight the text
+		                                textArea.setCaretPosition(pos + findLength);
+		                                textArea.moveCaretPosition(pos);
+		                                // Move the search position beyond the current match
+		                                pos += findLength;
+		                            }
+
+		                        } catch (Exception exp) {
+		                            exp.printStackTrace();
+		                        }
+
+		                    }
 						}
 					});
 				}
