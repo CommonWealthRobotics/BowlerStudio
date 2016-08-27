8d3d3b3610934fdc1d9fd89c7516f9ecc497db4a
madhephaestus <mad.hephaestus@gmail.com>
2016-08-27 11:41:16 -0400

    Adding rotory and prismatic options

--- src/main/java/com/neuronrobotics/sdk/addons/kinematics/FirmataLink.java
+++ src/main/java/com/neuronrobotics/sdk/addons/kinematics/FirmataLink.java
@@ -6,18 +6,19 @@ import org.firmata4j.IOEvent;
 import org.firmata4j.Pin;
 import org.firmata4j.PinEventListener;
 
+import com.neuronrobotics.sdk.common.DeviceManager;
+
 public class FirmataLink extends AbstractLink implements  PinEventListener{
 
-	
-	private FirmataBowler device;
 	private Pin pin;
 
 	public FirmataLink(LinkConfiguration arg0,FirmataBowler device) throws InterruptedException, IllegalArgumentException, IOException {
 		super(arg0);
-		this.device = device;
 		
 		pin = device.getFirmataDevice().getPin(arg0.getHardwareIndex());
-		pin.setMode(Pin.Mode.SERVO); // our listeners will get event about this change
+		pin.setMode(Pin.Mode.SERVO); 
+		// our listeners will get event about this change
+		pin.addEventListener(this);
 	}
 
 	@Override
@@ -37,28 +38,51 @@ public class FirmataLink extends AbstractLink implements  PinEventListener{
 		try {
 			pin.setValue((long) getTargetValue());
 		} catch (IllegalStateException | IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public double getCurrentPosition() {
-		// TODO Auto-generated method stub
 		return pin.getValue();
 	}
 
 	@Override
 	public void onModeChange(IOEvent event) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void onValueChange(IOEvent event) {
-		// TODO Auto-generated method stub
 		fireLinkListener(getCurrentPosition());
 		
 	}
-
+	
+	public static void addLinkFactory(){
+		INewLinkProvider lp = new INewLinkProvider() {
+			@Override
+			public AbstractLink generate(LinkConfiguration config) {
+				FirmataBowler dev = (FirmataBowler)DeviceManager
+						.getSpecificDevice(
+								FirmataBowler.class, 
+								config.getDeviceScriptingName()
+								);
+				if(dev!= null)
+					try {
+						return new FirmataLink(config, dev);
+					} catch (IllegalArgumentException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				return null;
+			}
+		};
+		LinkFactory.addLinkProvider("firmata-servo-rotory", lp);
+		LinkFactory.addLinkProvider("firmata-servo-prismatic", lp);
+	}
 }
