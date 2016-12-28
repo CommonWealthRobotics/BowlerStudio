0e4ecebddb09587c537e95080d07831e684e7a85
Kevin Harrington <mad.hephaestus@gmail.com>
2016-12-27 23:04:20 -0500

    Fixing the link widjet so it displays current value
    The current value was not being displayed on the screen.

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/LinkSliderWidget.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/LinkSliderWidget.java
@@ -39,7 +39,8 @@ import net.java.games.input.Component;
 import net.java.games.input.Controller;
 import net.java.games.input.Event;
 
-public class LinkSliderWidget extends Group implements  IJointSpaceUpdateListenerNR,IJInputEventListener,IOnEngineeringUnitsChange, ILinkListener {
+public class LinkSliderWidget extends Group
+		implements  IJInputEventListener, IOnEngineeringUnitsChange, ILinkListener {
 	private AbstractKinematicsNR device;
 	private DHParameterKinematics dhdevice;
 
@@ -52,119 +53,111 @@ public class LinkSliderWidget extends Group implements  IJointSpaceUpdateListene
 	private double seconds;
 	private String paramsKey;
 	private AbstractLink abstractLink;
+	private EngineeringUnitsSliderWidget slide;
 
-
-	
-	
-	
 	public LinkSliderWidget(int linkIndex, DHLink dhlink, AbstractKinematicsNR d) {
 
 		this.linkIndex = linkIndex;
 		this.device = d;
-		if(DHParameterKinematics.class.isInstance(device)){
-			dhdevice=(DHParameterKinematics)device;
+		if (DHParameterKinematics.class.isInstance(device)) {
+			dhdevice = (DHParameterKinematics) device;
 		}
 
 		abstractLink = device.getAbstractLink(linkIndex);
-		
-		
 
 		TextField name = new TextField(abstractLink.getLinkConfiguration().getName());
 		name.setMaxWidth(100.0);
 		name.setOnAction(event -> {
 			abstractLink.getLinkConfiguration().setName(name.getText());
 		});
-		
-		setpoint = new EngineeringUnitsSliderWidget(this, 
-		abstractLink.getMinEngineeringUnits(), 
-		abstractLink.getMaxEngineeringUnits(), 
-		device.getCurrentJointSpaceVector()[linkIndex], 
-		180,dhlink.getLinkType()==DhLinkType.ROTORY?"degrees":"mm");
-		
-		
-		GridPane panel = new GridPane();
-		
-		panel.getColumnConstraints().add(new ColumnConstraints(30)); // column 1 is 75 wide
-		panel.getColumnConstraints().add(new ColumnConstraints(120)); // column 1 is 75 wide
-		panel.getColumnConstraints().add(new ColumnConstraints(120)); // column 2 is 300 wide
-		
-		
 
-		panel.add(	new Text("#"+linkIndex), 
-				0, 
-				0);
-		panel.add(	name, 
-				1, 
-				0);
-		panel.add(	setpoint, 
-				2, 
-				0);
+		setpoint = new EngineeringUnitsSliderWidget(this, 
+													abstractLink.getMinEngineeringUnits(),
+													abstractLink.getMaxEngineeringUnits(), 
+													device.getCurrentJointSpaceVector()[linkIndex],
+													180,
+													dhlink.getLinkType() == DhLinkType.ROTORY ? "degrees" : "mm");
+
+		GridPane panel = new GridPane();
+
+		panel.getColumnConstraints().add(new ColumnConstraints(30)); // column 1
+																		// is 75
+																		// wide
+		panel.getColumnConstraints().add(new ColumnConstraints(120)); // column
+																		// 1 is
+																		// 75
+																		// wide
+		panel.getColumnConstraints().add(new ColumnConstraints(120)); // column
+																		// 2 is
+																		// 300
+																		// wide
+
+		panel.add(new Text("#" + linkIndex), 0, 0);
+		panel.add(name, 1, 0);
+		panel.add(setpoint, 2, 0);
 
 		getChildren().add(panel);
 		abstractLink.addLinkListener(this);
-		
-	}
-	
+		//device.addJointSpaceListener(this);
 
-	public void changed(ObservableValue<? extends Boolean> observableValue,
-            Boolean wasChanging,
-            Boolean changing) {
-
-        }
-
-	@Override
-	public void onJointSpaceUpdate(AbstractKinematicsNR source, double[] joints) {
-		Platform.runLater(()->{
-			try{
-				setpoint.setValue(joints[linkIndex]);
-			}catch(ArrayIndexOutOfBoundsException ex){
-				return;
-			}
-		});
-		
-		
 	}
 
-	@Override
-	public void onJointSpaceTargetUpdate(AbstractKinematicsNR source,
-			double[] joints) {
-		// TODO Auto-generated method stub
-		
-	}
+	// public void changed(ObservableValue<? extends Boolean> observableValue,
+	// Boolean wasChanging,
+	// Boolean changing) {
+	//
+	// }
+//
+//	@Override
+//	public void onJointSpaceUpdate(AbstractKinematicsNR source, double[] joints) {
+//
+//		try {
+//			setpoint.setValue(joints[linkIndex]);
+//		} catch (ArrayIndexOutOfBoundsException ex) {
+//			return;
+//		}
+//		
+//
+//	}
+//
+//	@Override
+//	public void onJointSpaceTargetUpdate(AbstractKinematicsNR source, double[] joints) {
+//		// TODO Auto-generated method stub
+//		System.out.println("targe update");
+//	}
+//
+//	@Override
+//	public void onJointSpaceLimit(AbstractKinematicsNR source, int axis, JointLimit event) {
+//		// TODO Auto-generated method stub
+//		System.out.println("limit update");
+//
+//	}
 
-	@Override
-	public void onJointSpaceLimit(AbstractKinematicsNR source, int axis,
-			JointLimit event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private void controllerLoop(){
+	private void controllerLoop() {
 		seconds = .1;
-		if(getGameController()!=null || stop==false){
+		if (getGameController() != null || stop == false) {
 
-
-			if(!stop){ 
-				jogTHreadHandle.setToSet(slider+setpoint.getValue(), seconds);
+			if (!stop) {
+				jogTHreadHandle.setToSet(slider + setpoint.getValue(), seconds);
 			}
 
-			FxTimer.runLater(
-					Duration.ofMillis((int)(seconds*1000.0)) ,() -> {
-						controllerLoop();
-					});
+			FxTimer.runLater(Duration.ofMillis((int) (seconds * 1000.0)), () -> {
+				controllerLoop();
+			});
 		}
 	}
 
-	private class jogThread extends Thread{
-		private boolean controlThreadRunning=false;
+	private class jogThread extends Thread {
+		private boolean controlThreadRunning = false;
 
-		private double toSeconds=seconds;
+		private double toSeconds = seconds;
 
 		private double newValue;
-		public void run(){
+
+		public void run() {
 			setName("Jog Link Slider");
-			while(device.isAvailable() ){
-				if(controlThreadRunning){
+			while (device.isAvailable()) {
+				if (controlThreadRunning) {
 					try {
 						device.setDesiredJointAxisValue(linkIndex, newValue, toSeconds);
 						setpoint.setValue(newValue);
@@ -172,94 +165,91 @@ public class LinkSliderWidget extends Group implements  IJointSpaceUpdateListene
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					controlThreadRunning=false;
+					controlThreadRunning = false;
 				}
-				ThreadUtil.wait((int) (toSeconds*1000));
+				ThreadUtil.wait((int) (toSeconds * 1000));
 			}
 		}
 
-		public void setToSet(double newValue,double toSeconds) {
+		public void setToSet(double newValue, double toSeconds) {
 
 			this.newValue = newValue;
 			this.toSeconds = toSeconds;
-			controlThreadRunning=true;
+			controlThreadRunning = true;
 		}
-		
+
 	}
-	
+
 	public void setGameController(BowlerJInputDevice controller) {
 		this.controller = controller;
-		if(	controller!= null && 	jogTHreadHandle ==null){
+		if (controller != null && jogTHreadHandle == null) {
 			jogTHreadHandle = new jogThread();
 			jogTHreadHandle.start();
 		}
-		
-		if(controller!=null){
+
+		if (controller != null) {
 			Controller hwController = controller.getController();
 			paramsKey = hwController.getName();
-			System.err.println("Controller key: "+paramsKey);
+			System.err.println("Controller key: " + paramsKey);
 			getGameController().clearListeners();
 			getGameController().addListeners(this);
 			controllerLoop();
 		}
 	}
 
-
 	public BowlerJInputDevice getGameController() {
 		return controller;
 	}
 
-
 	@Override
-	public void onEvent(Component comp, net.java.games.input.Event event,
-			float value, String eventString){
-		
-		if(comp.getName().toLowerCase().contentEquals((String) ConfigurationDatabase.getObject(paramsKey, "jogLink", "x")))
-			slider=-value;
+	public void onEvent(Component comp, net.java.games.input.Event event, float value, String eventString) {
 
-		if(Math.abs(slider)<.01)
-			slider=0;
-		if(slider==0) {
-			//System.out.println("Stoping on="+comp.getName());
-			stop=true;
-		}else
-			stop=false;
+		if (comp.getName().toLowerCase()
+				.contentEquals((String) ConfigurationDatabase.getObject(paramsKey, "jogLink", "x")))
+			slider = -value;
+
+		if (Math.abs(slider) < .01)
+			slider = 0;
+		if (slider == 0) {
+			// System.out.println("Stoping on="+comp.getName());
+			stop = true;
+		} else
+			stop = false;
 	}
-
 
 	@Override
 	public void onSliderMoving(EngineeringUnitsSliderWidget source, double newAngleDegrees) {
 		// TODO Auto-generated method stub
 		try {
 			device.setDesiredJointAxisValue(linkIndex, setpoint.getValue(), 0);
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		};
-		
-	}
-	
-	@Override
-	public void onSliderDoneMoving(EngineeringUnitsSliderWidget source,
-			double newAngleDegrees) {
-		
+		}
+		;
+
 	}
 
+	@Override
+	public void onSliderDoneMoving(EngineeringUnitsSliderWidget source, double newAngleDegrees) {
+
+	}
 
 	@Override
 	public void onLinkLimit(AbstractLink arg0, PIDLimitEvent arg1) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void onLinkPositionUpdate(AbstractLink arg0, double arg1) {
 		// TODO Auto-generated method stub
-		if (this.isFocused())
+		try {
 			setpoint.setValue(arg1);
+		} catch (ArrayIndexOutOfBoundsException ex) {
+			return;
+		}
 	}
-
 
 }
