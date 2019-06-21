513b644753d80885ea52055a2e0b0876ffbc85cf
Kevin Harrington <mad.hephaestus@gmail.com>
2019-06-21 17:34:54 -0400

    load menue with known message

--- src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioMenuWorkspace.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioMenuWorkspace.java
@@ -15,17 +15,18 @@ public class BowlerStudioMenuWorkspace {
 	private static HashMap<String, Object> workspaceData = null;
 	private static final int maxMenueSize = 15;
 	private static boolean sorting = false;
+
 	public static void init(Menu workspacemenu) {
-		if(workspacemenu==null)
+		if (workspacemenu == null)
 			throw new RuntimeException();
-		workspaceMenu = workspacemenu; 
+		workspaceMenu = workspacemenu;
 		loginEvent();
 
 	}
-	
+
 	public static void loginEvent() {
 		workspaceData = ConfigurationDatabase.getParamMap("workspace");
-		for(String o:workspaceData.keySet()) {
+		for (String o : workspaceData.keySet()) {
 			try {
 				ScriptingEngine.pull(o);
 			} catch (IOException e) {
@@ -34,24 +35,24 @@ public class BowlerStudioMenuWorkspace {
 			}
 		}
 		sort();
-		ConfigurationDatabase.save();
 	}
-	
-	public static void add(String url ) {
-		add( url,BowlerStudioMenu.gitURLtoMessage(url));
+
+	public static void add(String url) {
+		add(url, BowlerStudioMenu.gitURLtoMessage(url));
 	}
 
 	@SuppressWarnings("unchecked")
-	public static void add(String url,String menueMessage ) {
-		if (menueMessage == null || menueMessage.length()<2)
+	public static void add(String url, String menueMessage) {
+		if (menueMessage == null || menueMessage.length() < 2)
 			throw new RuntimeException();
 		ArrayList<String> data;
-		synchronized(workspaceData) {
+		synchronized (workspaceData) {
 			if (workspaceData.get(url) == null) {
 				data = new ArrayList<String>();
 				data.add(menueMessage);
 				data.add(new Long(System.currentTimeMillis()).toString());
 				workspaceData.put(url, data);
+				System.out.println("Workspace add: "+url);
 			}
 		}
 		data = (ArrayList<String>) workspaceData.get(url);
@@ -63,14 +64,17 @@ public class BowlerStudioMenuWorkspace {
 
 	@SuppressWarnings("unchecked")
 	private static void sort() {
-		if(sorting)
+		if (sorting)
 			return;
-		sorting=true;
+		sorting = true;
 		System.out.println("Sorting workspace...");
 		try {
 			ArrayList<String> myOptions = new ArrayList<String>();
-			for(String o:workspaceData.keySet()) {
-				myOptions.add(o);
+			synchronized (workspaceData) {
+				for (String o : workspaceData.keySet()) {
+					//System.out.println("Opt: "+o);
+					myOptions.add(o);
+				}
 			}
 			ArrayList<String> menu = new ArrayList<>();
 			while (myOptions.size() > 0) {
@@ -88,35 +92,48 @@ public class BowlerStudioMenuWorkspace {
 				}
 				String removedURL = (String) myOptions.remove(bestIndex);
 				if (menu.size() < maxMenueSize) {
-					
+
 					// clone all repos from git
 					try {
-						//ScriptingEngine.pull(removedURL);
+						// ScriptingEngine.pull(removedURL);
 						menu.add(removedURL);
-					}catch(Exception e) {
+					} catch (Exception e) {
 						// repo is broken or missing
+						e.printStackTrace();
+						System.out.println("Removing from workspace: " + removedURL);
+						synchronized (workspaceData) {
+							workspaceData.remove(removedURL);
+						}
+					}
+
+				} else {
+					System.out.println("Removing from workspace: " + removedURL);
+					synchronized (workspaceData) {
 						workspaceData.remove(removedURL);
 					}
-					
-				}else {
-					workspaceData.remove(removedURL);
 				}
 			}
 			Platform.runLater(() -> {
-				if(workspaceMenu.getItems()!=null)
+				if (workspaceMenu.getItems() != null)
 					workspaceMenu.getItems().clear();
-				new Thread(()->{
+				
+				new Thread(() -> {
 					for (String url : menu) {
-						BowlerStudioMenu.setUpRepoMenue(workspaceMenu,url, false,false);
+							BowlerStudioMenu.setUpRepoMenue(workspaceMenu, 
+									url, 
+									false, 
+									false,
+									((ArrayList<String>) workspaceData.get(url)).get(0));
+						
 					}
-					sorting=false;
+					sorting = false;
 				}).start();
 			});
-			
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-
+		ConfigurationDatabase.save();
 	}
 
 }
