6ddc3bde42979925c16934c195cb27f3acdc769d
Kevin Harrington <mad.hephaestus@gmail.com>
2019-06-27 14:10:18 -0400

    cleanup

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/JogKinematicsDevice.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/JogKinematicsDevice.java
@@ -1,19 +1,7 @@
 package com.neuronrobotics.bowlerstudio.creature;
 
-import javafx.application.Platform;
-import javafx.embed.swing.SwingNode;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-
 import com.neuronrobotics.bowlerstudio.tabs.AbstractBowlerStudioTab;
 import com.neuronrobotics.sdk.addons.kinematics.AbstractKinematicsNR;
-import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 import com.neuronrobotics.sdk.common.BowlerAbstractDevice;
 
 public class JogKinematicsDevice   extends AbstractBowlerStudioTab{
