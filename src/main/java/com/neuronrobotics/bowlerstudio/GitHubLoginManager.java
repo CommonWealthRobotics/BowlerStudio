1c89f06f9c0f7ebdddefa08d45bf0f16a0f53998
Kevin Harrington <mad.hephaestus@gmail.com>
2019-11-27 17:16:41 -0500

    adding 2 factor authentication
    Address #6

--- src/main/java/com/neuronrobotics/bowlerstudio/GitHubLoginManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/GitHubLoginManager.java
@@ -1,5 +1,8 @@
 package com.neuronrobotics.bowlerstudio;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import com.neuronrobotics.bowlerstudio.assets.BowlerStudioResourceFactory;
 import com.neuronrobotics.bowlerstudio.scripting.GithubLoginFX;
 import com.neuronrobotics.bowlerstudio.scripting.IGitHubLoginManager;
@@ -14,70 +17,80 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class GitHubLoginManager implements IGitHubLoginManager {
-	private  boolean loginWindowOpen = false;
-	private  GithubLoginFX githublogin = null;
-	private  Stage stage;
+	private boolean loginWindowOpen = false;
+	private GithubLoginFX githublogin = null;
+	private Stage stage;
 	private boolean AnonSelected = false;
 	private String[] creds;
-	public GitHubLoginManager(){
+
+	public GitHubLoginManager() {
 
 	}
+
 	@SuppressWarnings("restriction")
 	@Override
 	public String[] prompt(String username) {
-		if (AnonSelected){
+		if (AnonSelected) {
 			return null;
 		}
 		boolean loginWas = loginWindowOpen;
-		
+
 		if (githublogin != null)
 			githublogin.reset();
-		if(stage==null){						
+		if (stage == null) {
 			if (!loginWas && githublogin != null)
 				githublogin.reset();
 			githublogin = null;
-			//System.out.println("Calling login from BowlerStudio "+username);
+			// System.out.println("Calling login from BowlerStudio "+username);
 			// new RuntimeException().printStackTrace();
 			FXMLLoader fxmlLoader = BowlerStudioResourceFactory.getGithubLogin();
-			if(fxmlLoader==null)
+			if (fxmlLoader == null)
 				return null;
 			Parent root = fxmlLoader.getRoot();
 			if (githublogin == null) {
 				githublogin = fxmlLoader.getController();
 				githublogin.reset();
 				Platform.runLater(() -> {
-					if(!loginWindowOpen){
+					if (!loginWindowOpen) {
 						githublogin.reset();
 						githublogin.getUsername().setText(username);
 						stage = new Stage();
 						stage.setTitle("GitHub Login");
-						//stage.initModality(Modality.APPLICATION_MODAL);
+						// stage.initModality(Modality.APPLICATION_MODAL);
 						githublogin.setStage(stage, root);
 						stage.centerOnScreen();
-					
+
 						loginWindowOpen = true;
 						stage.setAlwaysOnTop(true);
 						stage.show();
-						
-						stage =null;
+
+						stage = null;
 					}
 				});
 			}
 		}
 		// setContent(root);
-		do{
-			//System.err.println("Waiting for user login");
+		do {
+			// System.err.println("Waiting for user login");
 
 			ThreadUtil.wait(100);
-		}while (!githublogin.isDone()) ;
+		} while (!githublogin.isDone());
 		creds = githublogin.getCreds();
-		//System.err.println(" login = "+creds);
-		if(creds == null){
-			//System.err.println("Anon mode");
-			AnonSelected=true;
+		// System.err.println(" login = "+creds);
+		if (creds == null) {
+			// System.err.println("Anon mode");
+			AnonSelected = true;
 		}
 		loginWindowOpen = false;
 		return creds;
+	}
+
+	@Override
+	public String twoFactorAuthCodePrompt() {
+		JFrame jframe = new JFrame();
+		String answer = JOptionPane.showInputDialog(jframe, "Enter 2 factor auth code:");
+		jframe.dispose();
+		return answer;
 	}
 
 }
