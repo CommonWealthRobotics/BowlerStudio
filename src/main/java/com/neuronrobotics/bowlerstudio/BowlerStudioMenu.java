f33712c806ee1db0e3a406074f23a4824febc3ed
Kevin Harrington <mad.hephaestus@gmail.com>
2020-01-04 12:33:54 -0500

    1.0.9

--- src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioMenu.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioMenu.java
@@ -558,48 +558,48 @@ public class BowlerStudioMenu implements MenuRefreshEvent {
 			}
 		};
 	}
-	//Helper function to get the previous commit.
-	public static RevCommit getPrevHash(RevCommit commit, Repository repo)  throws  IOException {
-
-	    try (RevWalk walk = new RevWalk(repo)) {
-	        // Starting point
-	        walk.markStart(commit);
-	        int count = 0;
-	        for (RevCommit rev : walk) {
-	            // got the previous commit.
-	            if (count == 1) {
-	                return rev;
-	            }
-	            count++;
-	        }
-	        walk.dispose();
-	    }
-	    //Reached end and no previous commits.
-	    return null;
-	}
-	//Helper gets the diff as a string.
-	private static String getDiffOfCommit( RevCommit oldCommit,RevCommit newCommit, Repository repo,Git git) throws IOException {
-	    //Use treeIterator to diff.
-	    AbstractTreeIterator oldTreeIterator = getCanonicalTreeParser(oldCommit,git);
-	    AbstractTreeIterator newTreeIterator = getCanonicalTreeParser(newCommit,git);
-	    OutputStream outputStream = new ByteArrayOutputStream();
-	    try (DiffFormatter formatter = new DiffFormatter(outputStream)) {
-	        formatter.setRepository(git.getRepository());
-	        formatter.format(oldTreeIterator, newTreeIterator);
-	    }
-	    String diff = outputStream.toString();
-	    return diff;
-	}
-	//Helper function to get the tree of the changes in a commit. Written by Rüdiger Herrmann
-	private static AbstractTreeIterator getCanonicalTreeParser(ObjectId commitId,Git git) throws IOException {
-	    try (RevWalk walk = new RevWalk(git.getRepository())) {
-	        RevCommit commit = walk.parseCommit(commitId);
-	        ObjectId treeId = commit.getTree().getId();
-	        try (ObjectReader reader = git.getRepository().newObjectReader()) {
-	            return new CanonicalTreeParser(null, reader, treeId);
-	        }
-	    }
-	}
+//	//Helper function to get the previous commit.
+//	public static RevCommit getPrevHash(RevCommit commit, Repository repo)  throws  IOException {
+//
+//	    try (RevWalk walk = new RevWalk(repo)) {
+//	        // Starting point
+//	        walk.markStart(commit);
+//	        int count = 0;
+//	        for (RevCommit rev : walk) {
+//	            // got the previous commit.
+//	            if (count == 1) {
+//	                return rev;
+//	            }
+//	            count++;
+//	        }
+//	        walk.dispose();
+//	    }
+//	    //Reached end and no previous commits.
+//	    return null;
+//	}
+//	//Helper gets the diff as a string.
+//	private static String getDiffOfCommit( RevCommit oldCommit,RevCommit newCommit, Repository repo,Git git) throws IOException {
+//	    //Use treeIterator to diff.
+//	    AbstractTreeIterator oldTreeIterator = getCanonicalTreeParser(oldCommit,git);
+//	    AbstractTreeIterator newTreeIterator = getCanonicalTreeParser(newCommit,git);
+//	    OutputStream outputStream = new ByteArrayOutputStream();
+//	    try (DiffFormatter formatter = new DiffFormatter(outputStream)) {
+//	        formatter.setRepository(git.getRepository());
+//	        formatter.format(oldTreeIterator, newTreeIterator);
+//	    }
+//	    String diff = outputStream.toString();
+//	    return diff;
+//	}
+//	//Helper function to get the tree of the changes in a commit. Written by Rüdiger Herrmann
+//	private static AbstractTreeIterator getCanonicalTreeParser(ObjectId commitId,Git git) throws IOException {
+//	    try (RevWalk walk = new RevWalk(git.getRepository())) {
+//	        RevCommit commit = walk.parseCommit(commitId);
+//	        ObjectId treeId = commit.getTree().getId();
+//	        try (ObjectReader reader = git.getRepository().newObjectReader()) {
+//	            return new CanonicalTreeParser(null, reader, treeId);
+//	        }
+//	    }
+//	}
 	public static String slugify(String input) {
 		return Normalizer.normalize(input, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "")
 				.replaceAll("[^ \\w]", "").trim().replaceAll("\\s+", "-").toLowerCase(Locale.ENGLISH);
@@ -660,25 +660,27 @@ public class BowlerStudioMenu implements MenuRefreshEvent {
 					String dateString = formatSimple.format( new Date()   );
 
 					newBranchItem.setOnAction(event1 -> {
-						promptForNewBranch(newBranchName1+"-"+dateString,"Create a new Branch from "+newBranchName1,newBranch -> {
-							new Thread() {
-								public void run() {
-									try {
-										String slugify = slugify(newBranch);
-										System.out.println("Creating Branch " + slugify);
-										ScriptingEngine.newBranch(url, slugify);
-										resetMenueForLoadingFiles(orgBranches, loading, thisEvent);
-										resetMenueForLoadingFiles(orgCommits, loadingCommits, loadCommitsEvent);
-									} catch (IOException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									} catch (GitAPIException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									}
-								}
-							}.start();
-						});
+						promptForNewBranch(newBranchName1+"-"+dateString,
+								"Create a new Branch from "+newBranchName1,
+								newBranch -> {
+									new Thread() {
+										public void run() {
+											try {
+												String slugify = slugify(newBranch);
+												System.out.println("Creating Branch " + slugify);
+												ScriptingEngine.newBranch(url, slugify);
+												resetMenueForLoadingFiles(orgBranches, loading, thisEvent);
+												resetMenueForLoadingFiles(orgCommits, loadingCommits, loadCommitsEvent);
+											} catch (IOException e) {
+												// TODO Auto-generated catch block
+												e.printStackTrace();
+											} catch (GitAPIException e) {
+												// TODO Auto-generated catch block
+												e.printStackTrace();
+											}
+										}
+									}.start();
+								});
 
 					});
 					Platform.runLater(() -> {
