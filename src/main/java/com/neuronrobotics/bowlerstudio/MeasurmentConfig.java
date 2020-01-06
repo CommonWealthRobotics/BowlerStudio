4f7d5879cb3f606852b32f24b501d4ca3154fd22
Kevin Harrington <mad.hephaestus@gmail.com>
2020-01-05 19:35:35 -0500

    Basic editing of data

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/MeasurmentConfig.java
@@ -0,0 +1,26 @@
+package com.neuronrobotics.bowlerstudio;
+
+import java.util.HashMap;
+
+public class MeasurmentConfig {
+	private String key;
+	private HashMap<String, Object> configs;
+	public MeasurmentConfig(String key,HashMap<String, Object> configs){
+		this.configs = configs;
+		this.setKey(key);
+		System.out.println("Adding Measurment "+key+" "+getMeasurment());
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getMeasurment() {
+		return configs.get(key).toString();
+	}
+	public void setMeasurment(String measurment) {
+		System.out.println("Setting field "+key+" to "+measurment);
+		configs.put(key, measurment);
+	}
+}
--- src/main/java/com/neuronrobotics/bowlerstudio/NewVitaminWizardController.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/NewVitaminWizardController.java
@@ -4,9 +4,13 @@ package com.neuronrobotics.bowlerstudio;
  */
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import com.neuronrobotics.bowlerstudio.assets.AssetFactory;
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import com.neuronrobotics.sdk.addons.kinematics.JavaFXInitializer;
 
 import javafx.application.Application;
@@ -16,10 +20,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -62,11 +75,31 @@ public class NewVitaminWizardController  extends Application {
     private AnchorPane measurmentPane; // Value injected by FXMLLoader
 
     @FXML // fx:id="measurmentsTable"
-    private TableView<String> measurmentsTable; // Value injected by FXMLLoader
-    
+    private TableView<MeasurmentConfig> measurmentsTable; // Value injected by FXMLLoader
+	@FXML 
+	private TextField newTypeNameField;
+	@FXML 
+	private TextField newSizeField;
+    @FXML // fx:id="nameColumn"
+    private TableColumn<MeasurmentConfig, String> nameColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="measurmentColumn"
+    private TableColumn<MeasurmentConfig, String> measurmentColumn; // Value injected by FXMLLoader
+    @FXML // fx:id="isMotor"
+    private CheckBox isMotor; // Value injected by FXMLLoader
+
+    @FXML // fx:id="isShaft"
+    private CheckBox isShaft; // Value injected by FXMLLoader
+    @FXML // fx:id="editExisting"
+    private CheckBox editExisting; // Value injected by FXMLLoader
+
 	private static INewVitaminCallback callback=null;
 
 	private static Stage primaryStage;
+	private String typeOfVitamin = null;
+	private String sizeOfVitamin=null;
+
+	private String sizeOfVitaminString;
 
 	@FXML
     void onConfirmAndCreate(ActionEvent event) {
@@ -82,6 +115,88 @@ public class NewVitaminWizardController  extends Application {
 
     @FXML
     void onConfirmSize(ActionEvent event) {
+    	if(!editExisting.isSelected()) {
+	    	sizeOfVitaminString = newSizeField.getText();
+	    	if(sizeOfVitaminString.length()<2) {
+				Platform.runLater(() -> {
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("No name specified!");
+					alert.setHeaderText("Names must be at least 2 charrectors long");
+					alert.setContentText("Try again...");
+					alert.showAndWait();
+				});
+				return;
+			}
+			String slug = BowlerStudioMenu.slugify(sizeOfVitaminString);
+			if(!sizeOfVitaminString.contentEquals(slug)) {
+				Platform.runLater(() -> {
+					Alert alert = new Alert(AlertType.WARNING);
+					alert.setTitle("Name Format Wrong");
+					alert.setHeaderText("Name must be without spaces or special chars");
+					alert.setContentText("Changed to "+slug+" confirm to continue...");
+					alert.showAndWait();
+					Platform.runLater(() -> newSizeField.setText(slug));
+				});
+				return;
+			}
+			sizeOfVitaminString=slug;
+    	}else {
+    		sizeOfVitaminString=sizeComboBox.getSelectionModel().getSelectedItem();
+    	}
+    		
+		Platform.runLater(() ->nameColumn.setCellValueFactory(new PropertyValueFactory<MeasurmentConfig, String>("key")));
+		Platform.runLater(() ->measurmentColumn.setCellValueFactory(new PropertyValueFactory<MeasurmentConfig, String>("measurment")));
+		Platform.runLater(() ->
+			measurmentColumn
+			.setCellFactory(
+	                TextFieldTableCell.forTableColumn())
+				);
+		
+		Platform.runLater(() ->measurmentsTable.getSelectionModel().cellSelectionEnabledProperty().set(true));
+		measurmentsTable.setEditable(true);
+		nameColumn.setEditable(false);
+		measurmentColumn.setEditable(true);
+		measurmentColumn.setOnEditCommit(ev -> {
+			final String value = ev.getNewValue() != null ? (String) ev.getNewValue() : (String) ev.getOldValue();
+			((MeasurmentConfig) ev.getTableView().getItems().get(ev.getTablePosition().getRow()))
+					.setMeasurment(value);
+			measurmentsTable.refresh();
+	    });
+		
+		
+		if(existingTypeRadio.isSelected()) {
+			if(!editExisting.isSelected()) {
+				boolean exists = false;
+				// for existing types check for existing sizes
+				ArrayList<String> sizes = Vitamins.listVitaminSizes(typeOfVitamin);
+				for(String size:sizes) {
+					if(size.contentEquals(sizeOfVitaminString))
+						exists=true;
+				}
+				if(exists) {
+					Platform.runLater(() -> {
+						Alert alert = new Alert(AlertType.WARNING);
+						alert.setTitle("Size already Exists");
+						alert.setHeaderText("Name must be unique");
+						alert.setContentText("Rename and confirm to continue...");
+						alert.showAndWait();
+					});
+					return;
+				}
+			}
+			new Thread(() -> {
+				HashMap<String, Object> configsOld = Vitamins.getConfiguration(typeOfVitamin, sizeComboBox.getSelectionModel().getSelectedItem());
+				HashMap<String, Object> configs = Vitamins.getConfiguration(typeOfVitamin, sizeOfVitaminString);
+				for(String key:configsOld.keySet())
+					configs.put(key,configsOld.get(key));
+				for(String key:configs.keySet()) {
+					Platform.runLater(() ->measurmentsTable.getItems().add(new MeasurmentConfig(key, configs)));
+				}
+			}).start();
+		
+			
+		}
+		
     	sizePane.setDisable(true);
         measurmentPane.setDisable(false);
         typePane.setDisable(true);
@@ -89,26 +204,118 @@ public class NewVitaminWizardController  extends Application {
 
     @FXML
     void onConfirmType(ActionEvent event) {
+
+    	if(newTypeRadio.isSelected()) {
+    		typeOfVitamin=newTypeNameField.getText();
+    		if(typeOfVitamin.length()<2) {
+    			Platform.runLater(() -> {
+    				Alert alert = new Alert(AlertType.WARNING);
+    				alert.setTitle("No name specified!");
+    				alert.setHeaderText("Names must be at least 2 charrectors long");
+    				alert.setContentText("Try again...");
+    				alert.showAndWait();
+    			});
+    			return;
+    		}
+    		String slug = BowlerStudioMenu.slugify(typeOfVitamin);
+    		if(!typeOfVitamin.contentEquals(slug)) {
+    			Platform.runLater(() -> {
+    				Alert alert = new Alert(AlertType.WARNING);
+    				alert.setTitle("Name Format Wrong");
+    				alert.setHeaderText("Name must be without spaces or special chars");
+    				alert.setContentText("Changed to "+slug+" confirm to continue...");
+    				alert.showAndWait();
+    				Platform.runLater(() -> newTypeNameField.setText(slug));
+    			});
+    			
+    			return;
+    		}
+    		typeOfVitamin=slug;
+    		sizeComboBox.setDisable(true);
+    		
+    	}else {
+    		typeOfVitamin=typeComboBox.getSelectionModel().getSelectedItem();
+    		ArrayList<String> sizes = Vitamins.listVitaminSizes(typeOfVitamin);
+    		for(String size:sizes) {
+    			sizeComboBox.getItems().add(size);
+    		}
+    		sizeComboBox.getSelectionModel().select(sizes.get(0));
+    	}
     	sizePane.setDisable(false);
         measurmentPane.setDisable(true);
         typePane.setDisable(true);
+        
     }
 
-    @FXML
+ 
+	@FXML
     void onNewMeasurment(ActionEvent event) {
+		HashMap<String, Object> configs = Vitamins.getConfiguration(typeOfVitamin, sizeOfVitaminString);
+		TextInputDialog dialog = new TextInputDialog("lengthOfThing");
+		dialog.setTitle("Add new measurment to "+typeOfVitamin);
+		dialog.setHeaderText("This measurment will be added to all instances of the vitamin");
+		dialog.setContentText("New measurment name:");
+
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		// The Java 8 way to get the response value (with lambda expression).
+		result.ifPresent(name -> { 
+			TextInputDialog dialog2 = new TextInputDialog("0.0");
+			dialog2.setTitle("Set value of "+name);
+			dialog2.setHeaderText("This value will be added to all instances of the vitamin");
+			dialog2.setContentText(name+" = ");
+
+			// Traditional way to get the response value.
+			Optional<String> result2 = dialog2.showAndWait();
+			result2.ifPresent(name2 -> { 
+				configs.put(name,name2);
+				measurmentsTable.getItems().add(new MeasurmentConfig(name, configs));
+				for(String size:Vitamins.listVitaminSizes(typeOfVitamin)) {
+					Vitamins.getConfiguration(typeOfVitamin, size).put(name,name2);
+				}
+			});
+			
+		});
+		
 
     }
 
     @FXML
     void onSelectExistingTypeMode(ActionEvent event) {
-
+    	newTypeNameField.setEditable(false);
+    	typeComboBox.setDisable(false);
     }
 
     @FXML
     void onSelectNewTypeMode(ActionEvent event) {
-
+    	newTypeNameField.setEditable(true);
+    	typeComboBox.setDisable(true);
+    	isShaft.setDisable(false);
+		isMotor.setDisable(false);
+    }
+    @FXML
+    void onEditExisting(ActionEvent event) {
+    	if(editExisting.isSelected()) {
+    		newSizeField.setEditable(false);
+    	}else {
+    		
+    		newSizeField.setEditable(true);
+    	}
     }
 
+    @FXML
+    void onIsMotor(ActionEvent event) {
+    	if(isMotor.isSelected())
+    		isShaft.setSelected(false);
+    }
+
+    @FXML
+    void onIsShaft(ActionEvent event) {
+    	
+    	if(isShaft.isSelected())
+    		isMotor.setSelected(false);
+    	
+    }
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert x1 != null : "fx:id=\"x1\" was not injected: check your FXML file 'newVitaminWizard.fxml'.";
@@ -121,7 +328,19 @@ public class NewVitaminWizardController  extends Application {
         
         sizePane.setDisable(true);
         measurmentPane.setDisable(true);
-        
+        ToggleGroup groupForType= new ToggleGroup();
+        existingTypeRadio.setSelected(true);
+        newTypeRadio.setSelected(false);
+        existingTypeRadio.setToggleGroup(groupForType);
+        newTypeRadio.setToggleGroup(groupForType);
+        newTypeNameField.setEditable(false);
+        ArrayList<String> types = Vitamins.listVitaminTypes();
+		for(String s:types) {
+			typeComboBox.getItems().add(s);
+		}
+		typeComboBox.getSelectionModel().select(types.get(0));
+		isShaft.setDisable(true);
+		isMotor.setDisable(true);
     }
     
     public static void launchWizard(INewVitaminCallback callback) throws Exception {
@@ -154,13 +373,17 @@ public class NewVitaminWizardController  extends Application {
 			
 			@Override
 			public void addVitaminType(String s) {
-				// TODO Auto-generated method stub
-				System.out.println("Add addVitaminType");
+				getTypeMenu(s);
+				ArrayList<String> sizes = Vitamins.listVitaminSizes(s);
+				for(String size:sizes) {
+					addSizesToMenu(size,s);
+				}
+				System.out.println("Add addVitaminType "+s);
 			}
 			
 			@Override
 			public void addSizesToMenu(String size, String type) {
-				System.out.println("Add addSizesToMenu");
+				System.out.println("Add addSizesToMenu "+type+" "+size );
 			}
 		});
     }
@@ -174,7 +397,7 @@ public class NewVitaminWizardController  extends Application {
 		loader.setClassLoader(getClass().getClassLoader());
 		root = loader.load();
 		Platform.runLater(() -> {
-			primaryStage.setTitle("New Vitamin Wizard");
+			primaryStage.setTitle("Edit Vitamins Wizard");
 
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
