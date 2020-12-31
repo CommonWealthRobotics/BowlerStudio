c4cd0a5208248a100163471bdb43e20d1b42a3ed
Kevin Harrington <mad.hephaestus@gmail.com>
2020-12-31 15:03:22 -0500

    adding API for fail over install

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/IExternalEditor.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/IExternalEditor.java
@@ -4,6 +4,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import com.neuronrobotics.bowlerstudio.BowlerStudio;
+
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public interface IExternalEditor {
 	
@@ -13,26 +23,69 @@ public interface IExternalEditor {
 	
 	String nameOfEditor();
 	
-	default void run(String finalCommand) {
-		System.out.println("Running:\n\n"+finalCommand+"\n\n");
+	URL getInstallURL() throws MalformedURLException;
+	
+	
+	default void run(File dir,String... finalCommand) {
+		List<String> asList = Arrays.asList(finalCommand);
+		System.out.println("Running:\n\n");
+		for(String s:asList)
+			System.out.print(s+" ");
+		System.out.println("\nIn "+dir.getAbsolutePath());
+		System.out.println("\n\n");
+		
 		new Thread(() -> {
 			try {
-				Process process = Runtime.getRuntime().exec(finalCommand);
-				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-				String line;
-				while ((line = reader.readLine()) != null && process.isAlive()) {
-					System.out.println(line);
-					try {
-						Thread.sleep(10);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				// creating the process
+				
+				ProcessBuilder pb = new ProcessBuilder(asList);
+				// setting the directory
+				pb.directory(dir);
+				// startinf the process
+				Process process = pb.start();
+				process.waitFor();
+				int ev = process.exitValue();
+				// System.out.println("Running "+commands);
+				if (ev != 0) {
+					System.out.println("ERROR PROCESS Process exited with " + ev);
 				}
-				reader.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				// for reading the ouput from stream
+				BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				BufferedReader errInput = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+				String s = null;
+				String e = null;
+				Thread.sleep(100);
+				while ((s = stdInput.readLine()) != null || (e = errInput.readLine()) != null) {
+					if (s != null)
+						System.err.println(s);
+					if (e != null)
+						System.err.println(e);
+					//
+				}
+				while (process.isAlive()) {
+					Thread.sleep(100);
+				}
+				System.out.println("");
+					
+			} catch (Throwable e) {
+				Platform.runLater(()->{
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle(nameOfEditor()+" is missing");
+					alert.setHeaderText("failed to run "+finalCommand);
+					alert.setContentText("Close to bring me to the install website");
+					alert.showAndWait();
+					new Thread(() -> {
+						try {
+							BowlerStudio.openExternalWebpage(getInstallURL());
+						} catch (MalformedURLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}).start();
+				});
+			
+
 			}
 		}).start();
 	}
--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/SVGExternalEditor.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/SVGExternalEditor.java
@@ -1,8 +1,18 @@
 package com.neuronrobotics.bowlerstudio.scripting;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.errors.NoWorkTreeException;
 
 import com.neuronrobotics.video.OSUtil;
+
+import sun.launcher.resources.launcher;
 
 public class SVGExternalEditor implements IExternalEditor {
 
@@ -17,15 +27,35 @@ public class SVGExternalEditor implements IExternalEditor {
 	@Override
 	public void launch(File file) {
 		String filename = file.getAbsolutePath();
-		if(OSUtil.isWindows()) {
-			filename="\""+filename+"\"";
-		}
-		run("inkscape "+filename);
+		//if(OSUtil.isWindows()) {
+		//	filename="\""+filename+"\"";
+		//}
+		try {
+			File dir = ScriptingEngine.locateGit(file).getRepository().getWorkTree();
+			run(dir,"inkscape",filename);
+			
+		} catch (NoWorkTreeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
 	}
-	
+	@Override
+	public URL getInstallURL() throws MalformedURLException {
+		return new URL("https://inkscape.org/release/inkscape-1.0.1/");
+	}
 	@Override
 	public String nameOfEditor() {
-		return "Inkscape Vector Editor";
+		return "Inkscape";
+	}
+	
+	public static void main(String [] args) throws InvalidRemoteException, TransportException, GitAPIException, IOException {
+		File f =ScriptingEngine.fileFromGit("https://github.com/Technocopia/Graphics.git", "Graphics/SimplifiedLogo/simplified logo.svg");
+		
+		new SVGExternalEditor().launch(f);
 	}
 
 }
