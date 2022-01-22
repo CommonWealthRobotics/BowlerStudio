95ac7ec450185772524f0fb7431e3e8f09293f2a
Kevin Harrington <mad.hephaestus@gmail.com>
2022-01-22 15:40:56 -0500

    add a graphic if its missing

--- src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioModularFrame.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/BowlerStudioModularFrame.java
@@ -388,7 +388,10 @@ public class BowlerStudioModularFrame {
 			Platform.runLater(() -> webTabs.get(newTab).requestFocus());
 		} else {
 			Platform.runLater(() -> {
-				if(newTab.getContent()==null||newTab.getGraphic()==null) {
+				if(newTab.getGraphic()==null) {
+					newTab.setGraphic(AssetFactory.loadIcon("Hardware-Config.png"));
+				}
+				if(newTab.getContent()==null) {
 					new Thread(()->{
 						try {
 							Thread.sleep(100);
@@ -396,7 +399,7 @@ public class BowlerStudioModularFrame {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						System.out.println("Tab "+newTab.getText()+" failed to load, retrying ");
+						System.out.println("Tab "+newTab.getText()+" failed to load, retrying "+newTab.getContent()+" "+newTab.getGraphic());
 						addTab( newTab,  b);
 					}).start();
 					return;
