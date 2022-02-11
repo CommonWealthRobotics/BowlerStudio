a8fb74b33bbb7a3a380e206ac088d18821ffb079
Kevin Harrington <mad.hephaestus@gmail.com>
2022-02-11 00:38:56 -0500

    removing the key from code

--- src/main/java/com/neuronrobotics/bowlerstudio/NameGetter.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/NameGetter.java
@@ -9,14 +9,17 @@ public class NameGetter  implements Supplier<String>{
 
 	@Override
 	public String get() {
-		String line = System.getProperty("API-SECRET");
-		if(line!=null)
-			return line;
-		JFrame jframe = new JFrame();
-		String answer = JOptionPane.showInputDialog(jframe, "Kevin Fucked up and left this in the release build\nEnter API secret");
-		jframe.dispose();
-		return answer;
-
+		String sec = "REPLACE_ME";
+		if(sec.contains("REPLACE")) {
+			String line = System.getProperty("API-SECRET");
+			if(line!=null)
+				return line;
+			JFrame jframe = new JFrame();
+			String answer = JOptionPane.showInputDialog(jframe, "Enter API secret");
+			jframe.dispose();
+			return answer;
+		}
+		return sec;
 	}
 
 }
