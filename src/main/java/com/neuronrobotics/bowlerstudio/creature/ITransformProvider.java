031c66b4d3a2aa01c6d33367915c66fa1a5538cb
Kevin Harrington <mad.hephaestus@gmail.com>
2024-04-14 15:12:08 -0400

    create a transform provider interface

---
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/ITransformProvider.java
@@ -0,0 +1,7 @@
+package com.neuronrobotics.bowlerstudio.creature;
+
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+
+public interface ITransformProvider {
+	TransformNR get();
+}
