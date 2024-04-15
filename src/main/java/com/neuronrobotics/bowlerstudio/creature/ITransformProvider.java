05dc3c9e4ca1a4f5cdcfe3561e0a9d1f7f722df3
Kevin Harrington <mad.hephaestus@gmail.com>
2024-04-15 11:41:12 -0400

    add a frame to the transform getter

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/ITransformProvider.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/ITransformProvider.java
@@ -1,7 +1,8 @@
 package com.neuronrobotics.bowlerstudio.creature;
 
+import com.neuronrobotics.sdk.addons.kinematics.VitaminLocation;
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 
 public interface ITransformProvider {
-	TransformNR get();
+	TransformNR get(VitaminLocation selectedVitamin);
 }
