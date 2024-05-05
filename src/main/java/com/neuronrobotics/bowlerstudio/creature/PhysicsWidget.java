cf32ae552c6dbe9adaa35dcd863cc2e2dd486ef6
Kevin Harrington <mad.hephaestus@gmail.com>
2024-05-05 13:37:54 -0400

    Add links to scripts to provide free and terrain objects

--- src/main/java/com/neuronrobotics/bowlerstudio/creature/PhysicsWidget.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/creature/PhysicsWidget.java
@@ -4,6 +4,7 @@ import com.neuronrobotics.bowlerstudio.BowlerStudio;
 import com.neuronrobotics.bowlerstudio.BowlerStudioController;
 import com.neuronrobotics.bowlerstudio.CreatureLab3dController;
 import com.neuronrobotics.bowlerstudio.assets.AssetFactory;
+import com.neuronrobotics.bowlerstudio.assets.ConfigurationDatabase;
 import com.neuronrobotics.bowlerstudio.physics.MuJoCoPhysicsManager;
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
 import com.neuronrobotics.sdk.addons.kinematics.LinkConfiguration;
@@ -18,6 +19,7 @@ import com.neuronrobotics.sdk.util.ThreadUtil;
 import eu.mihosoft.vrl.v3d.CSG;
 import javafx.application.Platform;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -46,6 +48,14 @@ public class PhysicsWidget extends GridPane  implements IMUUpdateListener {
 	private Set<CSG> oldParts=null;
 	private MobileBase base;
 	private  MuJoCoPhysicsManager mujoco;
+	private ComboBox<String> filesStatic;
+	private ComboBox<String> filesMoving;
+	private TextField gitStatic;
+	private TextField gitMoving;
+	private ArrayList<CSG> staticObjects=null;
+
+	private ArrayList<CSG> movingObjects=null;
+	
 	@SuppressWarnings("restriction")
 	public PhysicsWidget(MobileBase base){
 
@@ -70,10 +80,37 @@ public class PhysicsWidget extends GridPane  implements IMUUpdateListener {
 		controls.add(msLoopTime,4,0);
 		add(controls,0,0);
 		add(new Label("Static Objects:"),0,1);
-		
-		add(new Label("Moving Objects:"),0,4);
+		gitStatic = new TextField();
+		filesStatic = new ComboBox<>();
+		add(gitStatic,0,2);
+		add(filesStatic,1,2);
 
 		
+		add(new Label("Moving Objects:"),0,4);
+		gitMoving = new TextField();
+		filesMoving = new ComboBox<>();
+		add(gitMoving,0,5);
+		add(filesMoving,1,5);
+		
+		gitMoving.textProperty().addListener((observable, oldValue, newValue) -> {
+			validateInput();
+		});
+		gitStatic.textProperty().addListener((observable, oldValue, newValue) -> {
+			validateInput();
+		});
+		filesMoving.setDisable(true);
+		filesStatic.setDisable(true);
+		filesMoving.setOnAction(event->{
+			updateObjects();
+		});
+		filesStatic.setOnAction(event->{
+			updateObjects();
+		});
+		gitMoving.setText( ConfigurationDatabase.getObject("PhysicsWidget", "gitMoving", "").toString());
+		gitStatic.setText( ConfigurationDatabase.getObject("PhysicsWidget", "gitStatic", "").toString());
+		validateInput();
+
+
 		pauseresume.setDisable(true);
 		step.setDisable(true);
 		pauseresume.setOnAction(event->{
@@ -121,7 +158,7 @@ public class PhysicsWidget extends GridPane  implements IMUUpdateListener {
 								ScriptingEngine.getRepositoryCloneDirectory(
 										base.getGitSelfSource()[0]).getAbsolutePath()+"/physics/");
 						try {
-							mujoco = new MuJoCoPhysicsManager(base.getScriptingName(),bases,null,null,cache);
+							mujoco = new MuJoCoPhysicsManager(base.getScriptingName(),bases,movingObjects,staticObjects,cache);
 							
 						} catch (IOException | JAXBException e) {
 							// TODO Auto-generated catch block
@@ -148,6 +185,7 @@ public class PhysicsWidget extends GridPane  implements IMUUpdateListener {
 						BowlerStudioController.addObject(mujoco.getAllCSG(),null );
 						physicsThread = new Thread(){
 							public void run(){
+								ConfigurationDatabase.save();
 								try{
 									while(isRun()){
 										while(isPause() && isTakestep()==false){
@@ -183,6 +221,94 @@ public class PhysicsWidget extends GridPane  implements IMUUpdateListener {
 		});
 		
 	}
+	private void validateInput() {
+		validateInput(gitMoving,filesMoving,"gitMoving","movingObjects");
+		validateInput(gitStatic,filesStatic,"gitStatic","staticObjects");
+	}
+	private void validateInput(TextField text,ComboBox<String> box,String key,String key2) {
+		box.getItems().clear();
+		box.setDisable(true);
+		String text2 = text.getText();
+		if(!text2.endsWith(".git"))
+			return;
+		if(text2.length()<=5)
+			return;
+		try {
+			new java.net.URL(text2);
+		}catch(Exception ex) {
+			if(!text2.startsWith("git@")) {
+				ex.printStackTrace();
+				return;
+			}
+		}
+		box.setDisable(false);
+		new Thread(()->{
+			try {
+				ArrayList<String> files = ScriptingEngine.filesInGit(text2);
+				
+				for(String name:files) {
+					BowlerStudio.runLater(()->box.getItems().add(name));
+				}
+				ConfigurationDatabase.setObject("PhysicsWidget",key ,text2);
+				String file=ConfigurationDatabase.getObject("PhysicsWidget",key2 ,"").toString();
+				if(file.length()>0) {
+					box.getSelectionModel().select(file);
+					updateObjects();
+				}
+			} catch (Exception e) {
+				BowlerStudio.runLater(()->box.setDisable(true));
+				e.printStackTrace();
+			}
+		}).start();
+	}
+	private void updateObjects() {
+		new Thread(()->{
+			if(movingObjects!=null) {
+				for(CSG c:movingObjects) {
+					BowlerStudioController.removeObject(c);
+				}
+			}
+			if(staticObjects!=null) {
+				for(CSG c:staticObjects) {
+					BowlerStudioController.removeObject(c);
+				}
+			}
+			try {
+				movingObjects=null;
+				String selectedItem = filesMoving.getSelectionModel().getSelectedItem();
+				movingObjects=(ArrayList<CSG>) ScriptingEngine.gitScriptRun(gitMoving.getText(), selectedItem);
+				ConfigurationDatabase.setObject("PhysicsWidget","movingObjects" ,selectedItem);
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				staticObjects=null;
+				String selectedItem = filesStatic.getSelectionModel().getSelectedItem();
+				staticObjects=(ArrayList<CSG>) ScriptingEngine.gitScriptRun(gitStatic.getText(), selectedItem);
+				ConfigurationDatabase.setObject("PhysicsWidget","staticObjects" ,selectedItem);
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(movingObjects!=null) {
+				for(CSG c:movingObjects) {
+					BowlerStudioController.addObject(c,null);
+				}
+			}
+			if(staticObjects!=null) {
+				for(CSG c:staticObjects) {
+					BowlerStudioController.addObject(c,null);
+				}
+			}
+			
+		}).start();
+
+		
+	}
+	
 	private void stopPhysics() {
 		runstop.setGraphic(AssetFactory.loadIcon("Run.png"));
 		runstop.setText("Run");
