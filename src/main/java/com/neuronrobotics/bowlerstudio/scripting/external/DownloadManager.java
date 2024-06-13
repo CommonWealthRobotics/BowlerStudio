bde57f20c6ae7f245a27ccad4c53dc59a9a9a0d7
Kevin Harrington <mad.hephaestus@gmail.com>
2024-06-12 21:01:36 -0400

    only print the command once

--- src/main/java/com/neuronrobotics/bowlerstudio/scripting/external/DownloadManager.java
+++ src/main/java/com/neuronrobotics/bowlerstudio/scripting/external/DownloadManager.java
@@ -71,7 +71,7 @@ public class DownloadManager {
 		for(String s:tnp)
 			command+=(s+" ");
 		String cmd = command;
-		System.out.println(command);
+		out.println(command);
 		out.println("\nIn "+dir.getAbsolutePath());
 		out.println("\n\n");
 		String[] splited = command.split("\\s+");
