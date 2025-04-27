f5cb7d2fc10ae64a0f53fd21d24be34f4de92a8e
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-27 17:23:08 -0400

    update to the latest JVM from the json file

--- makeJar.sh
+++ makeJar.sh
@@ -1,16 +1,24 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+name=$(jq -r '."Linux-x64".name' jvm.json)
+type=$(jq -r '."Linux-x64".type' jvm.json)
+url=$(jq -r '."Linux-x64".url' jvm.json)
+
+# Verify the variables were set correctly
+echo "Name: $name"
+echo "Type: $type"
+echo "URL: $url"
 
 export ARCH=x86_64
-JVM=zulu8.78.0.19-ca-fx-jdk8.0.412-linux_x64
+JVM=$name
 set -e
-ZIP=$JVM.tar.gz
-export JAVA_HOME=$HOME/bin/java8/
+ZIP=$JVM.$type
+export JAVA_HOME=$HOME/bin/java17/
 if test -d $JAVA_HOME/$JVM/; then
   echo "$JAVA_HOME exists."
 else
 	rm -rf $JAVA_HOME
 	mkdir -p $JAVA_HOME
-	wget https://cdn.azul.com/zulu/bin/$ZIP 
+	wget $url$ZIP 
 	tar -xvzf $ZIP -C $JAVA_HOME
 	mv $JAVA_HOME/$JVM/* $JAVA_HOME/
 fi
