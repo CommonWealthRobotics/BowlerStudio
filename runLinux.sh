c2805a3cbc0692fdbd12e1cf65ea76405d8d6c36
Kevin Harrington <mad.hephaestus@gmail.com>
2025-04-27 17:46:42 -0400

    Update to Java 17 for building and running

---
+++ runLinux.sh
@@ -0,0 +1,45 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+name=$(jq -r '."Linux-x64".name' jvm.json)
+type=$(jq -r '."Linux-x64".type' jvm.json)
+url=$(jq -r '."Linux-x64".url' jvm.json)
+
+# Verify the variables were set correctly
+echo "Name: $name"
+echo "Type: $type"
+echo "URL: $url"
+
+export ARCH=x86_64
+JVM=$name
+set -e
+ZIP=$JVM.$type
+export JAVA_HOME=$HOME/bin/java17/
+if test -d $JAVA_HOME/$JVM/; then
+  echo "$JAVA_HOME exists."
+else
+	rm -rf $JAVA_HOME
+	mkdir -p $JAVA_HOME
+	wget $url$ZIP 
+	tar -xvzf $ZIP -C $JAVA_HOME
+	mv $JAVA_HOME/$JVM/* $JAVA_HOME/
+fi
+echo "Java home set to $JAVA_HOME"
+
+./gradlew clean shadowJar
+
+$JAVA_HOME/bin/java -Dprism.forceGPU=true --add-exports \
+	javafx.graphics/com.sun.javafx.css=ALL-UNNAMED \
+	--add-exports \
+	javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED \
+	--add-exports \
+	javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED \
+	--add-exports \
+	javafx.base/com.sun.javafx.event=ALL-UNNAMED \
+	--add-exports \
+	javafx.controls/com.sun.javafx.scene.control.skin.resources=ALL-UNNAMED \
+	--add-exports \
+	javafx.graphics/com.sun.javafx.util=ALL-UNNAMED \
+	--add-exports \
+	javafx.graphics/com.sun.javafx.scene.input=ALL-UNNAMED \
+	--add-opens \
+	javafx.graphics/javafx.scene=ALL-UNNAMED \
+	-jar build/libs/BowlerStudio.jar
