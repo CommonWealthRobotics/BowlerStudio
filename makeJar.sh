5ff5bbfa611151ab0b54df91941f737a87c7b47d
Kevin Harrington <mad.hephaestus@gmail.com>
2025-12-28 13:55:40 -0500

    update the CI builder

--- makeJar.sh
+++ makeJar.sh
@@ -1,10 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export ARCH=x86_64
-JVM=zulu8.78.0.19-ca-fx-jdk8.0.412-linux_x64
+JVM=zulu21.46.19-ca-fx-jdk21.0.9-linux_x64
 set -e
 ZIP=$JVM.tar.gz
-export JAVA_HOME=$HOME/bin/java8/
+export JAVA_HOME=$HOME/bin/java21/
 if test -d $JAVA_HOME/$JVM/; then
   echo "$JAVA_HOME exists."
 else
@@ -16,4 +16,4 @@ else
 fi
 echo "Java home set to $JAVA_HOME"
 
-./gradlew clean shadowJar
+./gradlew shadowJar
