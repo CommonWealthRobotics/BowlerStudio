5c1994cdd3fa978e6e58727ed5f424533e1f58a0
rondlh <77279634+rondlh@users.noreply.github.com>
2025-12-29 04:02:54 +0800

    Update makeJar.sh and runMac.sh
    Update makeJar.sh and runMac.sh

--- makeJar.sh
+++ makeJar.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export ARCH=x86_64
-JVM=zulu8.78.0.19-ca-fx-jdk8.0.412-linux_x64
+JVM=zulu21.46.19-ca-fx-jdk21.0.9-linux_x64
 set -e
 ZIP=$JVM.tar.gz
 export JAVA_HOME=$HOME/bin/java8/
