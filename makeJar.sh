98a66d72af9b3b240b742f05d6e5be108047df82
Kevin Harrington <mad.hephaestus@gmail.com>
2026-08-13 13:19:28 -0400

    update jdk

--- makeJar.sh
+++ makeJar.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export ARCH=x86_64
-JVM=zulu25.32.21-ca-fx-jdk25.0.2-linux_x64
+JVM=zulu25.36.15-ca-fx-jdk25.0.4-linux_x64
 set -e
 ZIP=$JVM.tar.gz
 export JAVA_HOME=$HOME/bin/java21/
